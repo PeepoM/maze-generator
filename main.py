d34a0546c210fd3fbf7758fadc21b1b2eec6a8eb
PeepoM <peter.marks@outlook.sk>
2021-07-23 12:18:02 +0200

    refactored main.py module to fit an OOP approach, introduced a new class Main

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from view import View
 from controller import Controller
 
 
-def main():
-    model = Model()
-    view = View(model.maze_rows, model.maze_cols, model.edge_length)
-    controller = Controller(model, view)
+class Main:
+    def __init__(self):
+        self.model = Model()
+        self.view = View(self.model.maze_rows, self.model.maze_cols,
+                         self.model.edge_length)
+        self.controller = Controller(self.model, self.view)
 
-    controller.game_loop()
+    def main(self):
+        self.controller.game_loop()
 
 
 if __name__ == '__main__':
-    main()
+    Main().main()
