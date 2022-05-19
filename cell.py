6b491562c8cc7d7519e3341b9c192ede9b0adea8
PeepoM <peter.marks@outlook.sk>
2022-05-19 13:54:31 +0200

    added functionalities for path generation

--- cell.py
+++ cell.py
@@ -1,16 +1,27 @@
 class Cell:
     EDGE_LENGTH = 20
-    
+
     def __init__(self, row, col):
         self.edge_length = Cell.EDGE_LENGTH
         self.row = row
         self.col = col
         self.walls = {"bottom": True, "right": True}
-    
+        self.parent = None
+
+    def reconstruct_path(self):
+        path = []
+        current = self
+
+        while current:
+            path.append(current)
+            current = current.parent
+
+        return list(reversed(path))
+
     @property
     def x(self):
         return self.col * self.edge_length
-    
+
     @property
     def y(self):
         return self.row * self.edge_length
